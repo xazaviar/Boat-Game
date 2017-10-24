d83785e2689e4a4698d4fa88913c8f19ae71946d
Joseph Ryan <xazaviar@gmail.com>
2017-10-24 15:21:06 -0400

    Color Picker
    Color Picker tester building

--- public/js/home.js
+++ public/js/home.js
@@ -1,24 +1,85 @@
-var patA = [50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250];
-var patB = [50,70,90,110,130,150,170,190,210,230,250];
-var patC = [50,80,110,140,170,200,230];
+var bases = [];
 
-var changeInterval = 1000;
+setTimeout(init,2);
 
-setTimeout(changeColors,2);
+function init(){
+    $("#color-picker1").hexColorPicker({
+        "container":"dialog",
+		"colorModel":"hsl",
+		"pickerWidth":300,
+		"size":7,
+		"style":"hex"
+    });
+    $("#color-picker2").hexColorPicker({
+        "container":"dialog",
+		"colorModel":"hsl",
+		"pickerWidth":300,
+		"size":7,
+		"style":"hex"
+    });
 
-function changeColors(){
-    var array = patA;
-    for(var i = 1; i <= 10; i++){
-        var r1 = parseInt(Math.random()*100)%array.length,
-            g1 = parseInt(Math.random()*100)%array.length,
-            b1 = parseInt(Math.random()*100)%array.length;
+    $('#baseForm').submit( function (e) {
+        e.preventDefault();
+        var base = $("#color-picker1").val();
+        var area = $("#color-picker2").val();
 
-        var r2 = parseInt(Math.random()*100)%array.length,
-            g2 = parseInt(Math.random()*100)%array.length,
-            b2 = parseInt(Math.random()*100)%array.length;
+        if(base!="" && area!=""){
+            var available = colorAvailable(base, area);
+            if(available==true){
+                bases.push({
+                    "baseColor":base,
+                    "areaColor":area
+                });
+                $("#color-picker1").css('background-color',"white");
+                $("#color-picker2").css('background-color',"white");
+                $("#color-picker1").val(null);
+                $("#color-picker2").val(null);
+                drawAllBases();
+            }else{
+                alert(available);
+            }
+        }else{
+            alert("Need two colors.");
+        }
+        return false;
+    });
+}
 
-        $("#c"+i).css('background-color','rgb('+array[r1]+','+array[g1]+','+array[b1]+')');
-        $("#c"+i+" .baseArea").css('background-color','rgb('+array[r2]+','+array[g2]+','+array[b2]+')');
+function drawAllBases(){
+    $('.mainArea').empty();
+    for(var b in bases){
+        $('.mainArea').append('<div id="c'+b+'" class="colorArea"><div class="baseArea"></div></div>');
+        $("#c"+b).css('background-color',bases[b].areaColor);
+        $("#c"+b+" .baseArea").css('background-color',bases[b].baseColor);
     }
-    setTimeout(changeColors,changeInterval);
+    //drawAvailableColors();
+}
+
+function colorAvailable(base, area){
+    if(base === area)
+        return "Base and area need to be different colors.";
+
+
+    var hex = area.replace('#','');
+    var r = parseInt(hex.substring(0,2), 16);
+    var g = parseInt(hex.substring(2,4), 16);
+    var b = parseInt(hex.substring(4,6), 16);
+    if(r+g+b < 140)
+        return "Area color is too dark";
+
+    hex = base.replace('#','');
+    var r2 = parseInt(hex.substring(0,2), 16);
+    var g2 = parseInt(hex.substring(2,4), 16);
+    var b2 = parseInt(hex.substring(4,6), 16);
+
+    if(Math.abs(r2-r)<20 && Math.abs(g2-g)<20 && Math.abs(b2-b)<20)
+        return "Base color is too similar to Area color";
+
+    for(var b in bases){
+        if(bases[b].baseColor==base && bases[b].areaColor==area){
+            return "Color Has been taken";
+        }
+    }
+
+    return true;
 }
